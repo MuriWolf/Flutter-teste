470845ba7febc4a19c74efb7a8c1442f4a791fbd
Murillo Pinheiro de Oliveria <murillop.o06@gmail.com>
2023-01-15 14:43:41 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/murillooliveira/flutterteste/fluttertest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.murillooliveira.flutterteste.fluttertest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/teste.dart
@@ -0,0 +1,7 @@
+// ignore_for_file: avoid_print
+
+void main() {
+  const num = 4;
+  print("Hello, world!");
+  print(num);
+}
